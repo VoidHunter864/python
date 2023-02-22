6774223b76b34ab0e6051d7d6d589c4ec9d4276b
Akashnil Garai <117503735+VoidHunter864@users.noreply.github.com>
2023-02-22 23:59:01 +0530

    first commit

---
+++ list_functions.py
@@ -0,0 +1,23 @@
+lucky_numbers = [4, 8, 5, 7, 6, 9]
+friends = ["Kevin", "Karen", "Jim", "Jim", "Mike", "Tim", "Oscar"]
+print(friends.count("Jim"))
+friends.sort()
+print(friends)
+friends2 = friends.copy()
+print(friends2)
+friends.reverse()
+print(friends)
+friends.extend(lucky_numbers)
+print(friends)
+friends.append("Creed")
+print(friends)
+print(friends.index("Kevin"))
+friends.pop
+print(friends)
+friends.insert(1, "Kelly")
+print(friends)
+friends.remove("Mike")
+print(friends)
+friends.clear()
+print(friends)
+
---
+++ tuples.py
@@ -0,0 +1,2 @@
+coordinates = [(4,5), (6,9), (80,34)]
+print(coordinates[1])
